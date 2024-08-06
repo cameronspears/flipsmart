49af6b30d53a59f7e852b09cc7eaeaf8e75d6e08
cameronspears <crspears@outlook.com>
2024-08-06 18:02:20 -0500

    Removed accessibility/tabbing feature on home page. Removed theme toggle. Default to dark mode on home page.

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,19 @@
 
 "use client";
 
-import React from 'react';
-import {ModeToggle} from "@/components/ModeToggle";
-import SearchBar from '@/components/SearchBar';
+import React from "react";
+import SearchBar from "@/components/SearchBar";
 
 const HomePage = () => {
-    return (
-        <div className="relative flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
-            <ModeToggle/>
-            <h1 className="text-4xl font-bold mb-6 text-gray-900 dark:text-gray-100 z-10">Flipsmart</h1>
-            <SearchBar/>
-        </div>
-    );
+  return (
+    <div className="relative flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+      {/* Removed ModeToggle */}
+      <h1 className="text-4xl font-bold mb-6 text-gray-900 dark:text-gray-100 z-10">
+        Flipsmart
+      </h1>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default HomePage;
